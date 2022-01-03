4604f5733c606ea168eb7507cfdf654bb46b8b64
oussamamth1 <oussamamethnani321@gmail.com>
2022-01-03 22:48:21 +0100

    first commit1

--- src/Security/TokenAuthenticator.php
+++ src/Security/TokenAuthenticator.php
@@ -4,7 +4,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\User\UserInterface;
-use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
+
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
